d88564a8f56f82e312ef178ed791c97e4566f42a
paul wisehart <paul@oldcode.org>
2011-06-20 20:42:27 -0400

    basic freemarker test works

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-java -cp target/standalone_test-0.0.1-SNAPSHOT.jar org.oldcode.freemarker.standalone_test.App
+#this way doesn't get the dependencies...
+#java -cp target/standalone_test-0.0.1-SNAPSHOT.jar org.oldcode.freemarker.standalone_test.App
+
+#args aren't used currently
+mvn exec:java -Dexec.mainClass="org.oldcode.freemarker.standalone_test.App" -Dexec.args="arg0 arg1 arg2"  
--- src/main/java/org/oldcode/freemarker/standalone_test/App.java
+++ src/main/java/org/oldcode/freemarker/standalone_test/App.java
@@ -1,13 +1,60 @@
 package org.oldcode.freemarker.standalone_test;
 
+import java.io.File;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) throws Exception {
+    	System.out.println("start...");
+    	
+        /* ------------------------------------------------------------------- */    
+        /* You should do this ONLY ONCE in the whole application life-cycle:   */    
+    
+        /* Create and adjust the configuration */
+        Configuration cfg = new Configuration();
+        cfg.setDirectoryForTemplateLoading(new File("/home/wise/data/workspace/standalone_test/templates"));
+        cfg.setObjectWrapper(new DefaultObjectWrapper());
+
+        /* ------------------------------------------------------------------- */    
+        /* You usually do these for many times in the application life-cycle:  */    
+                
+        /* Get or create a template */
+        Template temp = cfg.getTemplate("test.ftl");
+
+        /* Create a data-model */
+        Map<String, Object> root = new HashMap<String, Object>();
+        root.put("user", "Big Joe");
+        Map<String, String> latest = new HashMap<String, String>();
+        root.put("latestProduct", latest);
+        latest.put("url", "products/greenmouse.html");
+        latest.put("name", "green mouse");
+
+        /* Merge data-model with template */
+        Writer out = new OutputStreamWriter(System.out);
+        temp.process(root, out);
+        out.flush();
+        
+    	System.out.println("...stop");
     }
-}
+}  
+
+
+
+
+
+
+
+
+
